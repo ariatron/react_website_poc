66a0834e8cd315a09f726013a5eb3772f1568583
Ariana <ariana.r.lloyd@gmail.com>
2022-12-17 11:45:59 -0600

    Initial commit

---
+++ react-website/src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+return (
+	<div>
+	<h1>Send an emaill to something@aprivatedomain.com if you have questions!</h1>
+	</div>
+);
+};
+
+export default Contact;
