40f267744e383ce66b8191efdfd23334e15c7433
Ariana <ariana.r.lloyd@gmail.com>
2022-12-17 11:51:37 -0600

    Updating for my personal touch

--- react-website/src/pages/about.js
+++ react-website/src/pages/about.js
@@ -3,10 +3,8 @@ import React from "react";
 const About = () => {
 return (
 	<div>
-	<h1>
-		GeeksforGeeks is a Computer
-		Science portal for geeks.
-	</h1>
+	<h1>Welcome to the About page!</h1>
+	<p>This is the about page. There isn't a lot of information here but it's present all the same.</p>
 	</div>
 );
 };
